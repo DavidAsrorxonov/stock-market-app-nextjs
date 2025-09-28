11349a88affd3ac3be71d90824b64cce471e73e8
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-29 04:26:20 +0900

    set up react-hook-form; *not complete;

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "US",
+      investmentGoals: "Growth",
+      riskTolerance: "Medium",
+      preferredIndustry: "Technology",
+    },
+    mode: "onBlur",
+  });
+
+  return (
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+    </>
+  );
 };
 
 export default SignUpPage;
