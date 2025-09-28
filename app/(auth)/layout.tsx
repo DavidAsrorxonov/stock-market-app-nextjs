8e7bef522ccca2b8930d0a1c1ba5354f01a1f4eb
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-29 03:35:30 +0900

    import and show the dashboard image;

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -27,20 +27,29 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <div className="flex items-center justify-between">
             <cite className="auth-testimonial-author">- Ethan R.</cite>
             <p className="max-md:text-xs text-gray-500">Retail Investor</p>
+            <div className="flex items-center gap-0.5">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <Image
+                  src={"/assets/icons/star.svg"}
+                  alt="Star"
+                  key={star}
+                  width={20}
+                  height={20}
+                  className="w-5 h-5"
+                />
+              ))}
+            </div>
           </div>
+        </div>
 
-          <div className="flex items-center gap-0.5">
-            {[1, 2, 3, 4, 5].map((star) => (
-              <Image
-                src={"/assets/icons/star.svg"}
-                alt="Star"
-                key={star}
-                width={20}
-                height={20}
-                className="w-5 h-5"
-              />
-            ))}
-          </div>
+        <div className="flex-1 relative">
+          <Image
+            src={"/assets/images/dashboard.png"}
+            alt="Dashboard Preview"
+            width={1440}
+            height={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
         </div>
       </section>
     </main>
