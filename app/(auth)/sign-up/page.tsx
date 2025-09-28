ea9e115525bce73b5ee53de44179a16a5c5c7856
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-29 04:29:24 +0900

    start working on the sign up page UI;

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import React from "react";
 import { useForm } from "react-hook-form";
 
@@ -22,9 +23,29 @@ const SignUpPage = () => {
     mode: "onBlur",
   });
 
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h1 className="form-title">Sign Up & Personalize</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting
+            ? "Creating account..."
+            : "Start your investment journey"}
+        </Button>
+      </form>
     </>
   );
 };
