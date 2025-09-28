aa9c07e3643bcd438905e99a41c19e416abf796a
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-29 02:29:42 +0900

    create a special hook for the trading view widgets to avoid future repetitions;

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -6,17 +6,6 @@ import React, { useEffect, useRef, memo } from "react";
 function TradingViewWidget() {
   const container: any = useRef(null);
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = `
-        `;
-    container.current.appendChild(script);
-  }, []);
-
   return (
     <div
       className="tradingview-widget-container"
