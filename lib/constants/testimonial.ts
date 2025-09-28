76747dfe856e0d10279af0896e10e7b02e70f214
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-29 03:27:21 +0900

    create a layout page for signing in and up; create sign in and sign up pages; create testimonial constant and its type;

---
+++ lib/constants/testimonial.ts
@@ -0,0 +1,4 @@
+import { Testimonial } from "@/types/testimonial";
+
+export const testimonial: Testimonial =
+  "Signalist turned my watchlist into a winning list. The alerts are spot-on, and I feel more confident making moves in the market";
