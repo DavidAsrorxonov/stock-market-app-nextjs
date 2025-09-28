0f03a673b06858c1d402f9eaba93f85878003b83
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-29 01:00:32 +0900

    add the content for user dropdown; import and use the user dropdown component in the main header component;

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import NavItems from "./NavItems";
+import UserDropdown from "./UserDropdown";
 
 const Header = () => {
   return (
@@ -19,6 +20,8 @@ const Header = () => {
         <nav className="hidden sm:block">
           <NavItems />
         </nav>
+
+        <UserDropdown />
       </div>
     </header>
   );
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,7 +1,47 @@
+"use client";
+
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 const UserDropdown = () => {
-  return <div>UserDropdown</div>;
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    router.push("/sign-in");
+  };
+
+  const user: { name: string; email: string } = {
+    name: "John Doe",
+    email: "john@example.com",
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
+        ></Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserDropdown;
