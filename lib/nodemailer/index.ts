081dc13b299d40d6e69818d7c69df004ef10357e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-01 13:34:12 +0900

    add welcome email html texts tailored for different purposes; create an email sending function;

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,4 +1,6 @@
+import { WelcomeEmailData } from "@/types/welcomeEmail";
 import nodemailer from "nodemailer";
+import { WELCOME_EMAIL_TEMPLATE } from "./templates";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -7,3 +9,24 @@ export const transporter = nodemailer.createTransport({
     pass: process.env.NODEMAIL_APP_PASSWORD,
   },
 });
+
+export const sendWelcomeEmail = async ({
+  email,
+  name,
+  intro,
+}: WelcomeEmailData) => {
+  const htmlTemplate = WELCOME_EMAIL_TEMPLATE.replace("{{name}}", name).replace(
+    "{{intro}}",
+    intro
+  );
+
+  const mailOptions = {
+    from: "Signalist - Stock Market Alerts",
+    to: email,
+    subject: `Welcome to Signalist - your stock market toolkit is ready`,
+    text: "Thanks for joining Signalist! We're excited to have you aboard. Let's explore the world of stocks together!",
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
