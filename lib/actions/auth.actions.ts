06b2cab2d1483ef3c5c86af92ba9a1132d906ca2
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-03 22:19:23 +0900

    implement logging out functionality;

--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -3,6 +3,7 @@
 import { ReturnType } from "@/types/sendEmailFunctionReturn";
 import { auth } from "../better-auth/auth";
 import { inngest } from "../inngest/client";
+import { headers } from "next/headers";
 
 export const signUpWithEmail = async ({
   email,
@@ -45,6 +46,25 @@ export const signUpWithEmail = async ({
     return {
       success: false,
       message: "Sign up failed",
+    };
+  }
+};
+
+export const signOut = async (): Promise<ReturnType> => {
+  try {
+    await auth.api.signOut({
+      headers: await headers(),
+    });
+
+    return {
+      success: true,
+      message: "Sign out successful",
+    };
+  } catch (error) {
+    console.log("Sign out failed", error);
+    return {
+      success: false,
+      message: "Sign out failed",
     };
   }
 };
